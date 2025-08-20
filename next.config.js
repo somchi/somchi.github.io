791791b8e3f33deebab30e3adae9d0e028088e83
somchi <chisomike11@gmail.com>
2025-08-20 14:26:30 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,22 +1,27 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: "export",
+//   output: 'export',
 //   trailingSlash: true,
 //   images: {
 //     unoptimized: true,
 //   },
-//   basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-//   assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
+//   basePath: '',
+//   assetPrefix: '',
 //   env: {
-//     NEXT_PUBLIC_BASE_PATH:
-//       process.env.NODE_ENV === "production" ? "/portfolio" : "",
+//     NEXT_PUBLIC_BASE_PATH: '',
 //   },
 // };
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'somchi.github.io';
+
 const nextConfig = {
-  output: 'export', // <=== enables static exports
-  reactStrictMode: true,
-  basePath: '',
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : '',
+  },
 };
 
 module.exports = nextConfig;
--- src/app/_components/ui/command.tsx
+++ src/app/_components/ui/command.tsx
@@ -3,7 +3,7 @@ import { type DialogProps } from '@radix-ui/react-dialog';
 import { Command as CommandPrimitive } from 'cmdk';
 import { Search } from 'lucide-react';
 
-import { cn } from '@/app/libs/utils';
+import { cn } from '../../../app/libs/utils';
 import { Dialog, DialogContent } from './dialog';
 
 const Command = React.forwardRef<
