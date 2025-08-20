fe378dd5c42e9ae1ae3955756ca67e35249c87a9
somchi <chisomike11@gmail.com>
2025-08-20 13:03:22 +0100

    remove reduntant code

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+//   assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH:
+//       process.env.NODE_ENV === "production" ? "/portfolio" : "",
+//   },
+// };
+
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH:
-      process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  },
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
